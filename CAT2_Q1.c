349c21f0d6e8080363cf9f3ff7add48076033343
peterkiarie644-byte <peterkiarie644@gmail.com>
2025-11-05 13:28:22 +0300

    Implement main function to print scores array

---
+++ CAT2_Q1.c
@@ -0,0 +1,31 @@
+//Array-it is a fixed-size collection of similar data items stored in contagious memory location
+
+
+/*int scores[2][2] = {
+    {65, 92},
+    {84, 72}
+};
+
+int scores2[2][2] = {
+    {35, 70},
+    {59, 67}
+};
+*/
+#include <stdio.h>
+
+int main() {
+    int scores[2][2] = {
+        {65, 92},
+        {84, 72}
+    };
+
+    int i, j;
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("%d ", scores[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
